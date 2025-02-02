d885d031f13d570467541bdb9f335c49b2a35fa1
Najimandroid <najimandroid@gmail.com>
2025-02-02 22:23:01 +0100

    Update CustomBullets.cpp
    Second boss

--- Shoot_Em_Up/CustomBullets.cpp
+++ Shoot_Em_Up/CustomBullets.cpp
@@ -79,6 +79,113 @@ DefaultBoss::DefaultBoss(sf::Vector2f position_, sf::Vector2f direction_)
 	textureCoords = { 4, 0 };
 }
 
+//* SHRIMP BOSS BULLET *\\
+
+Shrimp::Shrimp(sf::Vector2f position_, sf::Vector2f direction_)
+{
+	position = position_;
+	direction = direction_;
+
+	sf::RectangleShape hitbox_(sf::Vector2f(WindowConfig::getInstance()->SIZE_Y / 17, WindowConfig::getInstance()->SIZE_Y / 17));
+	hitbox = hitbox_;
+	hitbox.setOrigin(sf::Vector2f(WindowConfig::getInstance()->SIZE_Y / 17 / 2.f, WindowConfig::getInstance()->SIZE_Y / 17 / 2.f));
+	hitbox.setPosition(position);
+
+	//std::cout << this->hitbox.getPosition().x << ", " << this->hitbox.getPosition().y << std::endl;
+
+	this->setDamage(20);
+	speed = 25;
+
+	lifeTimeMax = 5;
+
+	target = nullptr;
+
+	owner = nullptr;
+	color = sf::Color::Magenta;
+
+	textureCoords = { 4, 3 };
+	sinValue = .0f;
+}
+
+void Shrimp::updatePosition(float deltaTime)
+{
+	this->setVelocity({ WindowConfig::getInstance()->SIZE_X / 96 * deltaTime * this->getSpeed() * this->getDirection().x * 1920 / WindowConfig::getInstance()->SIZE_X
+				, WindowConfig::getInstance()->SIZE_X / 96 * deltaTime * this->getSpeed() * this->getDirection().y * 1920 / WindowConfig::getInstance()->SIZE_X });
+	this->setPosition(this->getPosition() + this->getVelocity());
+
+	this->setVelocity({ -LevelManager::getInstance()->SCROLLING_SPEED * deltaTime, (float)(sin(sinValue) * 4.f)});
+	this->setPosition(this->getPosition() + this->getVelocity());
+
+	this->getHitbox().setPosition(this->getPosition());
+
+	sinValue += .05f;
+}
+
+//* PUFFER *\\
+
+Puffer::Puffer(sf::Vector2f position_, sf::Vector2f direction_, Entity* owner_, Entity* target_)
+{
+	position = position_;
+	direction = direction_;
+	owner = owner_;
+	target = target_;
+
+	sf::RectangleShape hitbox_(sf::Vector2f(WindowConfig::getInstance()->SIZE_Y / 17, WindowConfig::getInstance()->SIZE_Y / 17));
+	hitbox = hitbox_;
+	hitbox.setOrigin(sf::Vector2f(WindowConfig::getInstance()->SIZE_Y / 17 / 2.f, WindowConfig::getInstance()->SIZE_Y / 17 / 2.f));
+	hitbox.setPosition(position);
+
+	//std::cout << this->hitbox.getPosition().x << ", " << this->hitbox.getPosition().y << std::endl;
+
+	this->setDamage(20);
+	speed = 15;
+
+	lifeTimeMax = 15;
+
+	
+	color = sf::Color::Magenta;
+
+	textureCoords = { 3, 2 };
+}
+
+void Puffer::updatePosition(float deltaTime)
+{
+
+	if (this->position.x < this->target->getPosition().x + 300) 
+	{
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), { -1, 0 }, 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), { 0, -1 }, 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), { 1, 0 }, 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), { 0, 1 }, 3, { 2, 2 });
+
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ -.5, .5 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ .5, -.5 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ .5, .5 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ -.5, -.5 }), 3, { 2, 2 });
+
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ -.25, .5 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ -.25, -.5 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ .25, .5 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ .25, -.5 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ -.5, -.25 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ .5, -.25 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ -.5, .25 }), 3, { 2, 2 });
+		BulletManager::getInstance()->spawnBullet(owner, this->getPosition(), normalize({ .5, .25 }), 3, { 2, 2 });
+
+
+		this->setPosition({ -900, 0 });
+	}
+
+	this->setVelocity({ WindowConfig::getInstance()->SIZE_X / 96 * deltaTime * this->getSpeed() * this->getDirection().x * 1920 / WindowConfig::getInstance()->SIZE_X
+					, WindowConfig::getInstance()->SIZE_X / 96 * deltaTime * this->getSpeed() * this->getDirection().y * 1920 / WindowConfig::getInstance()->SIZE_X });
+	this->setPosition(this->getPosition() + this->getVelocity());
+
+	this->setVelocity({ -LevelManager::getInstance()->SCROLLING_SPEED * deltaTime, 0 });
+	this->setPosition(this->getPosition() + this->getVelocity());
+
+	this->getHitbox().setPosition(this->getPosition());
+}
+
 //* HOMING BOSS BULLET *\\
 
 HomingBulletBoss::HomingBulletBoss(sf::Vector2f position_, Entity* target_, sf::Vector2f textureCoords_)
