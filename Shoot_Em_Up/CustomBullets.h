8b8845d5f4e68feb9a2f83f1a8529171d4beb6b5
Najimandroid <najimandroid@gmail.com>
2025-02-02 22:23:16 +0100

    Update CustomBullets.h
    Second boss

--- Shoot_Em_Up/CustomBullets.h
+++ Shoot_Em_Up/CustomBullets.h
@@ -28,6 +28,17 @@ public:
 	DefaultBoss(sf::Vector2f position_, sf::Vector2f direction_);
 };
 
+class Shrimp : public Bullet
+{
+private:
+	float sinValue;
+	Entity* target;
+public:
+	Shrimp(sf::Vector2f position_, sf::Vector2f direction_);
+
+	void updatePosition(float deltaTime) override;
+};
+
 class HomingBulletBoss : public Bullet
 {
 private:
@@ -35,6 +46,17 @@ private:
 	Entity* target;
 public:
 	HomingBulletBoss(sf::Vector2f position_, Entity* target_, sf::Vector2f textureCoords_);
+
+	void updatePosition(float deltaTime) override;
+};
+
+class Puffer : public Bullet
+{
+private:
+	Entity* target;
+	Entity* owner;
+public:
+	Puffer(sf::Vector2f position_, sf::Vector2f direction_, Entity* owner_, Entity* target_);
 
 	void updatePosition(float deltaTime) override;
 };
